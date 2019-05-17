a85705a4b3d4046f795f8bed48499c157c9ea4d6
Sergey Anisimov <sanisimov@moikot.com>
2019-05-18 00:25:14 +0200

    Add Prometheus matrics
    Added "Nil bot connection status" gauge. Zero is for "disconnected" and one for "connected".

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/nlopes/slack"
+	"net/http"
 	"os"
+
+	"github.com/nlopes/slack"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+const (
+	defaultPromAddr = ":9153"
+)
+
+var (
+	connStatus = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "nil_bot_connection_status",
+		Help: "Nil bot connection status",
+	})
 )
 
 func getEnv(name string) string {
@@ -14,12 +30,22 @@ func getEnv(name string) string {
 	return v
 }
 
+func getPromAddr() string {
+	v := os.Getenv("PROM_ADDR")
+	if v == "" {
+		return defaultPromAddr
+	}
+	return v
+}
+
 func main() {
 	token := getEnv("BOT_TOKEN")
 	api := slack.New(token)
 	rtm := api.NewRTM()
 
 	go rtm.ManageConnection()
+
+	go startHttp(getPromAddr())
 
 Loop:
 	for {
@@ -33,7 +59,11 @@ Loop:
 				fmt.Printf("error: invalid authentication\n")
 				break Loop
 
+			case *slack.ConnectedEvent:
+				connStatus.Set(1)
+
 			case *slack.DisconnectedEvent:
+				connStatus.Set(0)
 				if ev.Intentional {
 					break Loop
 				}
@@ -41,5 +71,13 @@ Loop:
 			default:
 			}
 		}
+	}
+}
+
+func startHttp(addr string) {
+	http.Handle("/metrics", promhttp.Handler())
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		panic("unable to create HTTP server, error: " + err.Error())
 	}
 }
